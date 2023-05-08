55bd2fc1462accb8b8dd8ef9b8384763293db45f
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2023-05-08 20:43:37 +0530

    Update IntroPage.js

--- src/components/IntroPage/IntroPage.js
+++ src/components/IntroPage/IntroPage.js
@@ -23,7 +23,9 @@ function IntroPage() {
                     <span> Mohamed Salahudeen</span>
                     <span> To leverage my engineering background and transferable skills to build a successful career in the field of Information Technology (IT). Seeking a challenging role that enables me to apply my analytical, problem-solving, and project management skills to develop innovative solutions and contribute to the growth of the organization. I aim to continuously enhance my knowledge and skills in IT through learning, collaboration, and hands-on experience, while embracing new technologies and trends.</span>
                 </div>
-                 <button className='btn intro-btn'><a style={{textDecoration:"none",color:"black"}} href='https://drive.google.com/file/d/1NsEX6BhXwiBoGXApT3FlcK_myLWfSQAV/view?usp=share_link' target='_blank'>Resume</a></button>
+                 <button className='btn intro-btn'><a style={{textDecoration:"none",color:"black"}}
+                  href='https://drive.google.com/file/d/15rfWM8jaVlbOhNgLBnIh9aWBi2Hyf0Hz/view?usp=share_link' target='_blank'>
+                    Resume</a></button>
             
                 <div className='intro-icons'>
               
