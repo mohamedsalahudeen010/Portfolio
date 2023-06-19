95fa9845b97547590e1393d948f7a42586e6e5d6
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2023-06-20 01:41:45 +0530

    Update project.js

--- src/Data/project.js
+++ src/Data/project.js
@@ -12,77 +12,96 @@ import p11 from "../components/image/project/pizzaApp.jpg"
 import p12 from "../components/image/project/Capstone.jpg"
 import p13 from "../components/image/project/Shine.png"
 
+import js from "../components/image/logo/js-logo.webp"
+
  export const project=[
     {
+        title:"Brewery Application",
         image:p1,
         netlify:"https://unique-griffin-0aa5f4.netlify.app/",
-        frontEnd:"https://github.com/mohamedsalahudeen010/WebCode1"
+        frontEnd:"https://github.com/mohamedsalahudeen010/WebCode1",
+        tech:["Javascript","HTML","CSS","DOM","Boostrap"]
     },
     {
+        title:"Country Cards Application",
         image:p2,
         netlify:"https://hilarious-valkyrie-083d59.netlify.app/",
-        frontEnd:"https://github.com/mohamedsalahudeen010/Fetch-task-card-creation-"
+        frontEnd:"https://github.com/mohamedsalahudeen010/Fetch-task-card-creation-",
+        tech:["Javascript","HTML","CSS","DOM","Boostrap"]
     },
     {
+        title:"Thirukkural Application",
         image:p3,
         netlify:"https://venerable-gaufre-16bbbb.netlify.app/",
-        frontEnd:"https://github.com/mohamedsalahudeen010/Thirukkural"
+        frontEnd:"https://github.com/mohamedsalahudeen010/Thirukkural",
+        tech:["Javascript","HTML","CSS","DOM","Boostrap"]
     },
     {
+        title:"Art Institute Application",
         image:p4,
         netlify:"https://lighthearted-cucurucho-100ecf.netlify.app",
-        frontEnd:"https://github.com/mohamedsalahudeen010/Art-Institute-of-Chicago"
+        frontEnd:"https://github.com/mohamedsalahudeen010/Art-Institute-of-Chicago",
+        tech:["Javascript","HTML","CSS","DOM","Boostrap"]
     },
     {
+        title:"Harry Potter Application",
         image:p5,
         netlify:"https://fluffy-sunshine-ceb9c8.netlify.app/",
-        frontEnd:"https://github.com/mohamedsalahudeen010/Harry-Potter"
+        frontEnd:"https://github.com/mohamedsalahudeen010/Harry-Potter",
+        tech:["Javascript","HTML","CSS","DOM","Boostrap"]
     },
     {
+        title:"MakeUp Shop Application",
         image:p6,
         netlify:"https://eclectic-salamander-afb833.netlify.app",
-        frontEnd:"https://github.com/mohamedsalahudeen010/Webcode---B42"
+        frontEnd:"https://github.com/mohamedsalahudeen010/Webcode---B42",
+        tech:["Javascript","HTML","CSS","DOM","Boostrap"]
     },
     {
+        title:"Product Cart Application",
         image:p7,
         netlify:"https://cool-sunburst-d44a0e.netlify.app/",
-        frontEnd:"https://github.com/mohamedsalahudeen010/Sports-Cart-React-"
+        frontEnd:"https://github.com/mohamedsalahudeen010/Sports-Cart-React-",
+        tech:["Javascript","HTML","CSS","Boostrap","React"]
     },
     {
-        image:p9,
-        netlify:"https://relaxed-twilight-d309ee.netlify.app/",
-        frontEnd:"https://github.com/mohamedsalahudeen010/Crud-React--Formik"
-       
-    },
-    {
-       
+        title:"Harry Potter School Crud Application",
         image:p8,
+        netlify:"https://relaxed-twilight-d309ee.netlify.app/",
+        frontEnd:"https://github.com/mohamedsalahudeen010/Crud-React--Formik",
+        tech:["Javascript","HTML","CSS","Boostrap","React","Formik"]
+       
+    },
+    {
+        title:"Students Crud Application ( FullStack )",
+        image:p9,
         netlify:"https://roaring-kheer-e775dc.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/Crud-Basic-app",
-        backEnd:"https://github.com/mohamedsalahudeen010/Basic-Crud-BackEnd-"
+        backEnd:"https://github.com/mohamedsalahudeen010/Basic-Crud-BackEnd-",
+        tech:["Javascript","HTML","CSS","Boostrap","React","Formik","NodeJs","Express","MongoDB"]
     },
     {
-        image:p10,
-        netlify:"https://euphonious-puffpuff-9d27ad.netlify.app/",
-        frontEnd:"https://github.com/mohamedsalahudeen010/urlShortener-frontend",
-        backEnd:"https://github.com/mohamedsalahudeen010/urlShortener-BackEnd"
-    },
-    {
+        title:"Pizza Ordering Application ( FullStack )",
         image:p11,
         netlify:"https://shimmering-mochi-ffa38e.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/WebCode-2-FrontEnd",
-        backEnd:"https://github.com/mohamedsalahudeen010/webcode-2-Backend"
+        backEnd:"https://github.com/mohamedsalahudeen010/webcode-2-Backend",
+        tech:["Javascript","HTML","CSS","Boostrap","React","Formik","NodeJs","Express","MongoDB","Mongoose","Redux"]
     },
     {
+        title:"Gold Rate Convertor Application ( FullStack )",
         image:p12,
         netlify:"https://tourmaline-figolla-345c51.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/Capstone-FrontEnd",
-        backEnd:"https://github.com/mohamedsalahudeen010/Capstone-BackEnd"
+        backEnd:"https://github.com/mohamedsalahudeen010/Capstone-BackEnd",
+        tech:["Javascript","HTML","CSS","Boostrap","React","Formik","Context API","NodeJs","Express","MongoDB","Mongoose"]
     },
     {
+        title:"Paint Shop Application ( FullStack )",
         image:p13,
         netlify:"https://shinetraders-app.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/shineTraders",
-        backEnd:"https://github.com/mohamedsalahudeen010/shineTraders-backEnd"
+        backEnd:"https://github.com/mohamedsalahudeen010/shineTraders-backEnd",
+        tech:["Javascript","HTML","CSS","Boostrap","React","Formik","NodeJs","Express","MongoDB","Mongoose","Redux"]
     }
 ]
