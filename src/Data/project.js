47e12ee9498f7ec401386297e392351171cf56b7
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2023-07-04 12:59:47 +0530

    Update project.js

--- src/Data/project.js
+++ src/Data/project.js
@@ -15,93 +15,113 @@ import p13 from "../components/image/project/Shine.png"
 import js from "../components/image/logo/js-logo.webp"
 
  export const project=[
-  {
+    {
         title:"Paint Shop Application ( FullStack )",
         image:p13,
         netlify:"https://shinetraders-app.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/shineTraders",
         backEnd:"https://github.com/mohamedsalahudeen010/shineTraders-backEnd",
-        tech:["Javascript","HTML","CSS","Boostrap","React","Formik","NodeJs","Express","MongoDB","Mongoose","Redux"]
+        tech:["Javascript","HTML","CSS","Boostrap","React","Formik","NodeJs","Express","MongoDB","Mongoose","Redux"],
+        description:"This is a MERN Full stack Application for Paint Shop, This application has products for users and stock updation for admin, It includes CRUD operations,"
     },
-  {
+    
+    {
         title:"Gold Rate Convertor Application ( FullStack )",
         image:p12,
         netlify:"https://tourmaline-figolla-345c51.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/Capstone-FrontEnd",
         backEnd:"https://github.com/mohamedsalahudeen010/Capstone-BackEnd",
-        tech:["Javascript","HTML","CSS","Boostrap","React","Formik","Context API","NodeJs","Express","MongoDB","Mongoose"]
+        tech:["Javascript","HTML","CSS","Boostrap","React","Formik","Context API","NodeJs","Express","MongoDB","Mongoose"],
+        description:"This is a Gold Rate Convertor MERN Full stack Application, assigned as a Capstone project, This application includes CRUD operations, Bcrypt Authorization, JWT Authentication, Bootstrap, ContextApi used for State Management, Formik used for Form control."
     },
-   {
+
+    {
         title:"Pizza Ordering Application ( FullStack )",
         image:p11,
         netlify:"https://shimmering-mochi-ffa38e.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/WebCode-2-FrontEnd",
         backEnd:"https://github.com/mohamedsalahudeen010/webcode-2-Backend",
-        tech:["Javascript","HTML","CSS","Boostrap","React","Formik","NodeJs","Express","MongoDB","Mongoose","Redux"]
+        tech:["Javascript","HTML","CSS","Boostrap","React","Formik","NodeJs","Express","MongoDB","Mongoose","Redux"],
+        description:"This is a Pizza ordering MERN Full stack Application, assigned as a Webcode project."
     },
-   {
+   
+    {
         title:"Students Crud Application ( FullStack )",
         image:p9,
         netlify:"https://roaring-kheer-e775dc.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/Crud-Basic-app",
         backEnd:"https://github.com/mohamedsalahudeen010/Basic-Crud-BackEnd-",
-        tech:["Javascript","HTML","CSS","Boostrap","React","Formik","NodeJs","Express","MongoDB"]
+        tech:["Javascript","HTML","CSS","Boostrap","React","Formik","NodeJs","Express","MongoDB"],
+        description:"This is a MERN Full stack Application, This Application has CRUD operations to maintain  Students data of School."
     },
-   {
+    
+    {
         title:"Harry Potter School Crud Application",
         image:p8,
         netlify:"https://relaxed-twilight-d309ee.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/Crud-React--Formik",
-        tech:["Javascript","HTML","CSS","Boostrap","React","Formik"]
-       
+        tech:["Javascript","HTML","CSS","Boostrap","React","Formik"],
+        description:"This is a React Front End application, This Application has CRUD operations to maintain Teachers and Students data of Hogwarts School. "
     },
+    
     {
         title:"Product Cart Application",
         image:p7,
         netlify:"https://cool-sunburst-d44a0e.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/Sports-Cart-React-",
-        tech:["Javascript","HTML","CSS","Boostrap","React"]
+        tech:["Javascript","HTML","CSS","Boostrap","React"],
+        description:"This is a Gold Rate Convertor MERN full stack Application, assigned as a Capstone project, This application includes CRUD operations, Bcrypt Authorization, JWT Authentication, Bootstrap, ContextApi used for State Management, Formik used for Form control."
     },
+  
     {
         title:"MakeUp Shop Application",
         image:p6,
         netlify:"https://eclectic-salamander-afb833.netlify.app",
         frontEnd:"https://github.com/mohamedsalahudeen010/Webcode---B42",
-        tech:["Javascript","HTML","CSS","DOM","Boostrap"]
+        tech:["Javascript","HTML","CSS","DOM","Boostrap"],
+        description:"This is a Gold Rate Convertor MERN full stack Application, assigned as a Capstone project, This application includes CRUD operations, Bcrypt Authorization, JWT Authentication, Bootstrap, ContextApi used for State Management, Formik used for Form control."
     },
-   {
+    
+    {
         title:"Harry Potter Application",
         image:p5,
         netlify:"https://fluffy-sunshine-ceb9c8.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/Harry-Potter",
-        tech:["Javascript","HTML","CSS","DOM","Boostrap"]
+        tech:["Javascript","HTML","CSS","DOM","Boostrap"],
+        description:"This is a Gold Rate Convertor MERN full stack Application, assigned as a Capstone project, This application includes CRUD operations, Bcrypt Authorization, JWT Authentication, Bootstrap, ContextApi used for State Management, Formik used for Form control."
     },
-   {
+   
+    {
         title:"Art Institute Application",
         image:p4,
         netlify:"https://lighthearted-cucurucho-100ecf.netlify.app",
         frontEnd:"https://github.com/mohamedsalahudeen010/Art-Institute-of-Chicago",
-        tech:["Javascript","HTML","CSS","DOM","Boostrap"]
+        tech:["Javascript","HTML","CSS","DOM","Boostrap"],
+        description:"This is a Gold Rate Convertor MERN full stack Application, assigned as a Capstone project, This application includes CRUD operations, Bcrypt Authorization, JWT Authentication, Bootstrap, ContextApi used for State Management, Formik used for Form control."
     },
-   {
+    {
         title:"Thirukkural Application",
         image:p3,
         netlify:"https://venerable-gaufre-16bbbb.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/Thirukkural",
-        tech:["Javascript","HTML","CSS","DOM","Boostrap"]
+        tech:["Javascript","HTML","CSS","DOM","Boostrap"],
+        description:"This is a Gold Rate Convertor MERN full stack Application, assigned as a Capstone project, This application includes CRUD operations, Bcrypt Authorization, JWT Authentication, Bootstrap, ContextApi used for State Management, Formik used for Form control."
     },
-  {
+    
+    {
         title:"Country Cards Application",
         image:p2,
         netlify:"https://hilarious-valkyrie-083d59.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/Fetch-task-card-creation-",
-        tech:["Javascript","HTML","CSS","DOM","Boostrap"]
+        tech:["Javascript","HTML","CSS","DOM","Boostrap"],
+        description:"This is a Gold Rate Convertor MERN full stack Application, assigned as a Capstone project, This application includes CRUD operations, Bcrypt Authorization, JWT Authentication, Bootstrap, ContextApi used for State Management, Formik used for Form control."
     },
     {
         title:"Brewery Application",
         image:p1,
         netlify:"https://unique-griffin-0aa5f4.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/WebCode1",
-        tech:["Javascript","HTML","CSS","DOM","Boostrap"]
-    }
+        tech:["Javascript","HTML","CSS","DOM","Boostrap"],
+        description:"This is a Gold Rate Convertor MERN full stack Application, assigned as a Capstone project, This application includes CRUD operations, Bcrypt Authorization, JWT Authentication, Bootstrap, ContextApi used for State Management, Formik used for Form control."
+    },
 ]
