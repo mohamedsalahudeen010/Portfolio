f6ee8f3301213d07a9c33eb8814b387dbb5125cb
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2023-07-04 12:07:33 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,27 +16,17 @@ function About() {
             <span>About Me</span>
             <div>Hi, I am Mohamed Salahudeen,</div>
           <span>
-          "I am a mechanical engineer with 5 years of experience in the industry, but I am currently transitioning to a career in IT. During my time as a mechanical engineer, I developed a strong foundation in problem-solving, analytical thinking, and project management. However, I realized that my passion lies in the ever-evolving world of technology.
+          "I am a mechanical engineer with 5 years of experience in the industry, but I am currently transitioning to a career in IT.
+           During my time as a mechanical engineer, I developed a strong foundation in problem-solving, analytical thinking, and 
+           project management. However, I realized that my passion lies in the ever-evolving world of technology.
        </span>
 
           <span>
-          As I began exploring the field of IT, I quickly discovered that many of the skills I developed as a mechanical engineer are transferable and valuable in the IT industry.
-    I took online courses and attended boot camps in ZenClass by GUVI to learn new skills and stay up-to-date with the latest technology trends.
+          As I began exploring the field of IT, I quickly discovered that many of the skills I developed as a mechanical engineer 
+          are transferable and valuable in the IT industry.
+          I took online Full stack developer program in Zen Class by GUVI and I completed successfully . 
+       
 
-          </span>
-          <span>
-          I am excited to use my skills and experience to make a positive impact in the field of IT. I am particularly interested 
-    in developing innovative solutions to complex problems and collaborating with others to create meaningful change.
-
-          </span>
-          <span>
-          In my free time, I enjoy exploring new technologies and experimenting with coding. I am always eager to learnnew things and take on new challenges. 
-    I believe that my combination of technical and problem-solving skills, as well as my passion for technology, make me a strong candidate for a career in IT."
-          </span>
-          <span>
-            
-As I make this transition into IT, I'm excited to bring my problem-solving skills, attention to detail, and strong work ethic to a new industry. I'm committed to continuously learning and growing in my new career and am excited about the opportunities ahead.
-   
           </span>
             </div>
             <div className='col-xs-12 col-md-6 about-right'   style={{width:"50%"}}>
