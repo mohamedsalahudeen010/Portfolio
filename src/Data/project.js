3053c7574fac86caff9f5640e111db0e8c462639
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2024-03-04 16:18:23 +0530

    Update project.js

--- src/Data/project.js
+++ src/Data/project.js
@@ -1,22 +1,46 @@
-import p1 from "../components/image/project/brewery-Dom.jpg"
-import p2 from "../components/image/project/country-Dom.jpg"
-import p3 from "../components/image/project/ThirukkuralApp-Dom.jpg"
 import p4 from "../components/image/project/ArtInstitute-Dom.jpg"
 import p5 from "../components/image/project/Harry-Dom.jpg"
 import p6 from "../components/image/project/makeUp-Dom.jpg"
 import p7 from "../components/image/project/cart-React.jpg"
 import p9 from "../components/image/project/Crud-React.jpg"
 import p8 from "../components/image/project/Harry-React.jpg"
-import p10 from "../components/image/project/UrlShortener-React.jpg"
 import p11 from "../components/image/project/pizzaApp.jpg"
 import p12 from "../components/image/project/Capstone.jpg"
 import p13 from "../components/image/project/Shine.png"
 import p14 from "../components/image/project/AkjStores.png"
+import p15 from "../components/image/project/IMDB_Admin.jpg"
+import p16 from "../components/image/project/IMDB_User.jpg"
 import js from "../components/image/logo/js-logo.webp"
 
 
 
  export const project=[
+    {
+        title:"IMDB Clone Admin UI ( FullStack )",
+        title2:"IMDB Admin UI",
+        image:p16,
+        netlify:"https://imdb-admi-ui.netlify.app/",
+        frontEnd:"https://github.com/mohamedsalahudeen010/IMDB-Admin-UI",
+        backEnd:"https://github.com/mohamedsalahudeen010/IMDB-Server",
+        tech:["Javascript","HTML","CSS","React","NodeJs","Express","MongoDB","Mongoose","Context API","Redux","Boostrap","Material UI","Formik-Yup"],
+        description:`This Application is a MERN Full stack Application of IMDB clone Admin UI, Provides
+        list of Movies with ratings,The platform includes user authentication,
+        product listing, search functionality, includes Bcrypt Authentication and
+        Json Web Token (JWT) Authorization.`
+    },
+    {
+        title:"IMDB Clone User UI ( FullStack )",
+        title2:"IMDB User UI",
+        image:p15,
+        netlify:"https://imdb-user-ui.netlify.app/",
+        frontEnd:"https://github.com/mohamedsalahudeen010/IMDB-User-UI",
+        backEnd:"https://github.com/mohamedsalahudeen010/IMDB-Server",
+        tech:["Javascript","HTML","CSS","React","NodeJs","Express","MongoDB","Mongoose","Context API","Redux","Boostrap","Material UI","Formik-Yup"],
+        description:`This Application is a MERN Full stack Application of IMDB clone User UI, Provides
+        list of Movies with ratings,The platform includes user authentication,
+        product listing, search functionality, includes Bcrypt Authentication and
+        Json Web Token (JWT) Authorization.`
+    },
     {
         title:"E-Commerce Application ( FullStack )",
         title2:"E-Commerce App",
@@ -24,7 +48,7 @@ import js from "../components/image/logo/js-logo.webp"
         netlify:"https://akjstores.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/AKJ-Stores",
         backEnd:"https://github.com/mohamedsalahudeen010/AKJBackEnd",
-        tech:["Javascript","HTML","CSS","Boostrap","React","Formik","NodeJs","Express","MongoDB","Mongoose","Context API","Redux"],
+        tech:["Javascript","HTML","CSS","React","NodeJs","Express","MongoDB","Mongoose","Context API","Redux","Boostrap","Material UI","Formik-Yup"],
         description:"This Application is a MERN Full stack Application for E commerce App, This application has products for users and stock updation for admin, It includes CRUD operations, includes Bcrypt Authentication and Json Web Token (JWT) Authorization."
     },
     {
@@ -34,18 +58,18 @@ import js from "../components/image/logo/js-logo.webp"
         netlify:"https://shinetraders-app.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/shineTraders",
         backEnd:"https://github.com/mohamedsalahudeen010/shineTraders-backEnd",
-        tech:["Javascript","HTML","CSS","Boostrap","React","Formik","NodeJs","Express","MongoDB","Mongoose",,"Context API","Redux"],
+        tech:["Javascript","HTML","CSS","React","NodeJs","Express","MongoDB","Mongoose","Context API","Redux","Boostrap","Formik-Yup"],
         description:"This Application is a MERN Full stack Application for Paint Shop, This application has products for users and stock updation for admin, It includes CRUD operations, includes Bcrypt Authentication and Json Web Token (JWT) Authorization."
     },
     
     {
         title:"Gold Rate Convertor Application ( FullStack )",
-        title2:"Gold Rate Convertor",
+        title2:"E-Commerce App",
         image:p12,
         netlify:"https://tourmaline-figolla-345c51.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/Capstone-FrontEnd",
         backEnd:"https://github.com/mohamedsalahudeen010/Capstone-BackEnd",
-        tech:["Javascript","HTML","CSS","Boostrap","React","Formik","Context API","NodeJs","Express","MongoDB","Mongoose"],
+        tech:["Javascript","HTML","CSS","React","NodeJs","Express","MongoDB","Mongoose","Context API","Redux","Boostrap","Formik-Yup"],
         description:"This Application is a Gold Rate Convertor MERN Full stack Application, assigned as a Capstone project, Welcome to the Gold Rate Convertor Application, a Web page designed to help you determine Gold prices using various weights, karats (e.g., 24K, 22K, 21K, 18K etc.), and currencies from around the world, includes Bcrypt Authentication and Json Web Token (JWT) Authorization."
     },
 
@@ -56,7 +80,7 @@ import js from "../components/image/logo/js-logo.webp"
         netlify:"https://shimmering-mochi-ffa38e.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/WebCode-2-FrontEnd",
         backEnd:"https://github.com/mohamedsalahudeen010/webcode-2-Backend",
-        tech:["Javascript","HTML","CSS","Boostrap","React","Formik","NodeJs","Express","MongoDB","Mongoose","Redux"],
+        tech:["Javascript","HTML","CSS","React","NodeJs","Express","MongoDB","Mongoose","Context API","Redux","Boostrap","Formik-Yup"],
         description:"This Application is a Pizza ordering MERN Full stack Application, assigned as a Webcode project, this contains user pizza ordering Interface and stock updation for Admin,Explore the menu for your favourite veg & non-veg pizzas and you can add toppings, includes Bcrypt Authentication and Json Web Token (JWT) Authorization."
     },
    
@@ -67,7 +91,7 @@ import js from "../components/image/logo/js-logo.webp"
         netlify:"https://roaring-kheer-e775dc.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/Crud-Basic-app",
         backEnd:"https://github.com/mohamedsalahudeen010/Basic-Crud-BackEnd-",
-        tech:["Javascript","HTML","CSS","Boostrap","React","Formik","NodeJs","Express","MongoDB"],
+        tech:["Javascript","HTML","CSS","React","NodeJs","Express","MongoDB","Boostrap","Formik-Yup"],
         description:"This Application is a MERN Full stack Application, This Application has CRUD operations to maintain Students data of School, includes Bcrypt Authentication and Json Web Token (JWT) Authorization."
     },
     
@@ -77,7 +101,7 @@ import js from "../components/image/logo/js-logo.webp"
         image:p8,
         netlify:"https://relaxed-twilight-d309ee.netlify.app/",
         frontEnd:"https://github.com/mohamedsalahudeen010/Crud-React--Formik",
-        tech:["Javascript","HTML","CSS","Boostrap","React","Formik"],
+        tech:["Javascript","HTML","CSS","Boostrap","React","Formik-Yup"],
         description:"This Application is a React Front End application, This Application has CRUD operations to maintain Teachers and Students data of Hogwarts School."
     },
     
@@ -119,25 +143,6 @@ import js from "../components/image/logo/js-logo.webp"
         frontEnd:"https://github.com/mohamedsalahudeen010/Art-Institute-of-Chicago",
         tech:["Javascript","HTML","CSS","DOM","Boostrap"],
         description:"This Application is a Front End application built by using DOM,This Application contains User Interface for Art Institute of Chicaco Museum."
-    },
-    {
-        title:"Thirukkural Application",
-        title2:"Thirukkural App",
-        image:p3,
-        netlify:"https://venerable-gaufre-16bbbb.netlify.app/",
-        frontEnd:"https://github.com/mohamedsalahudeen010/Thirukkural",
-        tech:["Javascript","HTML","CSS","DOM","Boostrap"],
-        description:"This Application is a Front End application built by using DOM,This Application contains User Interface for Art Thirukkural, designed for Study purpose."
-    },
-    
-    {
-        title:"Country Cards Application",
-        title2:"Country Cards App",
-        image:p2,
-        netlify:"https://hilarious-valkyrie-083d59.netlify.app/",
-        frontEnd:"https://github.com/mohamedsalahudeen010/Fetch-task-card-creation-",
-        tech:["Javascript","HTML","CSS","DOM","Boostrap"],
-        description:"This Application is a Front End application built by using DOM,This Application contains cards of various Countries of world, card includes capital, region, geographical location, claimate of particular country."
-    },
+    }
    
 ]
