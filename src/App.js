40d7b7badd696d8f1cfb488b7120f960ac5019dc
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2024-03-13 11:38:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import IntroPage from './components/IntroPage/IntroPage';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
 import Projects from './components/Projects/Projects';
+import ProjectsOld from './components/Projects/projectOld';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import { themeContext } from './context';
